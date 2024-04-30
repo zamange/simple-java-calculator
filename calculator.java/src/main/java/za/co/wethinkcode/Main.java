d6fc79821c58ca38c4e1f55b94bd5de13e665359
WTC <zngeyane023@student.wethinkcode.co.za>
2024-04-30 08:40:04 +0200

    read operator

--- calculator.java/src/main/java/za/co/wethinkcode/Main.java
+++ calculator.java/src/main/java/za/co/wethinkcode/Main.java
@@ -8,4 +8,6 @@ class Main {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.println("Choose an operator: +, -, *, or /");
+        char operator = input.next().charAt(0);
     }}
